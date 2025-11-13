b1a622acd8df703339e4bd33a2309f2ba881bce6
Atos Alves <atosalvesqueiroz@gmail.com>
2025-11-13 16:53:14 -0300

    :sparkles: adiciona botão para alternativas

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,9 +4,12 @@ import { Image, AlignCenter, AlignLeft, AlignRight, Bold, Italic } from "lucide-
 import type { Editor } from "@tiptap/react";
 
 export function Menu({ editor }: { editor: Editor }) {
+    const aplicarEstiloCitacao = () => editor.chain().focus().toggleSubscript().run();
+
+    const aplicarAlternativas = () => editor.chain().focus().toggleOrderedList().run();
+
     const aplicarNegrito = () => editor.chain().focus().toggleBold().run();
     const aplicarItalico = () => editor.chain().focus().toggleItalic().run();
-    const aplicarEstiloCitacao = () => editor.chain().focus().toggleSubscript().run();
 
     const aplicarAlinhamentoEsquerdo = () => editor.chain().focus().setTextAlign("left").run();
     const aplicarAlinhamentoCentro = () => editor.chain().focus().setTextAlign("center").run();
@@ -23,7 +26,10 @@ export function Menu({ editor }: { editor: Editor }) {
 
     return (
         <header className="flex items-center flex-wrap space-x-5">
-            <Button onClick={aplicarEstiloCitacao}>Citação</Button>
+            <div className="space-x-1">
+                <Button onClick={aplicarEstiloCitacao}>Citação</Button>
+                <Button onClick={aplicarAlternativas}>Alternativas</Button>
+            </div>
             <div className="space-x-1">
                 <Button onClick={aplicarNegrito}>
                     <Bold />
